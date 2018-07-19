353ef7679151878e16586df2e9f253d34c09a51d
zhennan.hu <zhennan.hu@phicomm.com>
2018-07-19 15:45:52 +0800

    run as app

--- src/main/java/com/example/hangman/HangmanClientApplication.java
+++ src/main/java/com/example/hangman/HangmanClientApplication.java
@@ -1,12 +1,25 @@
 package com.example.hangman;
 
+import com.example.hangman.service.PlayService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HangmanClientApplication {
+    @Value("${hangman.test.email}")
+    private String email;
 
     public static void main(String[] args) {
         SpringApplication.run(HangmanClientApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner play(PlayService playService) {
+        return args -> {
+            playService.play();
+        };
     }
 }
